5749730ce1ab03cf0bdaf5c4ac4db7de6358a5ae
Cameron Lee <cameronslee@hotmail.com>
2024-03-26 10:51:01 -0700

    refactor volume ops, cleanup

--- roy.py
+++ roy.py
@@ -1,4 +1,16 @@
-# Roy: A Simple Version Control System
+# 8888888b.                   
+# 888   Y88b                  
+# 888    888                  
+# 888   d88P .d88b.  888  888 
+# 8888888P" d88""88b 888  888 
+# 888 T88b  888  888 888  888 
+# 888  T88b Y88..88P Y88b 888 
+# 888   T88b "Y88P"   "Y88888 
+#                         888 
+#                    Y8b d88P 
+#                     "Y88P"  
+#
+# A Simple Version Control System
 
 import os
 import sys 
@@ -8,26 +20,28 @@ import hashlib
 from datetime import datetime
 import difflib
 
+### === Globals === ###
 # Hardcoded Config TODO remove
 CONFIG_USER_NAME = "foobar"
-
 # Denotes an initial commit
 NULL_SHA1 = 0000000000000000000000000000000000000000 
 
 ### === PATHS === ####
+CWD = os.getcwd()
 BASE_DIR = ".roy/" # base dir for Roy instance
 MASTER_VOLUME = ".roy/master/" 
+HEAD_REF_MASTER = ".roy/master/headref/" 
 CACHE_VOLUME = ".roy/cache/" 
 COMMIT_VOLUME = ".roy/commits/" 
 CHANGELOG_PATH = ".roy/changelog/" 
-CONFIG_PATH = ".roy/changelog/" 
+CONFIG_PATH = ".roy/.royconfig" 
 IGNORELIST_PATH = ".roy/.royignore"
 
-# Error Handles
+### === Error Handles === ###
 def perror(msg):
     print("error: " + msg)
 
-### === Data Stuctures - used for navigating change history === ###
+### === Structs - used for navigating change history === ###
 # Each commit will be a singly linked list node
 class Commit:
     def __init__(self, commit_id, author, commit_message, files, timestamp):
@@ -47,11 +61,7 @@ class VC:
         os.makedirs(self.tempdir, exist_ok=True)
         self.head_ref = head_ref
 
-# Build a linked list 
-# Reads master volume log, builds linked of each and returns a VC object
-# with the head_ref set
-def build_vc_instance():
-    pass
+### === Helpers === ###
 
 # Hash value is built from author name, timestamp and commit message
 def create_hash(name, timestamp, commit_msg):
@@ -89,11 +99,12 @@ def clear_cache_volume():
     assert len(os.listdir(CACHE_VOLUME)) == 0, "cache error: could not be cleared"
 
 # ain't this a funny function... FOR DEBUGGING PURPOSES ONLY. TODO
-def clear_master_volume():
-    for filename in os.listdir(MASTER_VOLUME):
+# Clear head ref in master volume
+def clear_headref_master():
+    for filename in os.listdir(HEAD_REF_MASTER):
         os.remove(filename)
 
-    assert len(os.listdir(MASTER_VOLUME)) == 0, "master error: could not be cleared"
+    assert len(os.listdir(HEAD_REF_MASTER)) == 0, "master error: could not be cleared"
 
 ### === Roy Command Functionality === ###
 def setup():
@@ -102,8 +113,15 @@ def setup():
         perror("roy version control system already set up")
         return
     
-    touch(MASTER_VOLUME)
+    mkdir(BASE_DIR)
+    mkdir(MASTER_VOLUME)
+    mkdir(HEAD_REF_MASTER)
+    mkdir(CACHE_VOLUME)
+    mkdir(CHANGELOG_PATH)
+    touch(CONFIG_PATH)
+    touch(IGNORELIST_PATH)
 
+    print("roy: setup complete")
 
 def diff_file(f1, f2):
     with open(f1, 'r') as f:
@@ -137,18 +155,16 @@ def diff():
     for l in lines:
         ignore_list.append(cwd + '/' + l.strip())
 
-    for filename in os.listdir(cwd):
+    for filename in os.listdir(CACHE_VOLUME):
         f1 = os.path.join(cwd, filename)
-        f2 = os.path.join(MASTER_VOLUME, filename)
+        f2 = os.path.join(cwd, HEAD_REF_MASTER + filename)
 
         if str(f1) in ignore_list:
             print("ignoring ", f1)
-            continue
         elif os.path.isfile(f1) and os.path.isfile(f2):
             print(diff_file(f1, f2))
             #add to changed_list
             changed_list.append(f1)
-
         else:
             perror("could not find matching file in working directory")
             print(f1, f2)
@@ -157,8 +173,28 @@ def diff():
 
 # Add file(s) to the local cache staging directory
 # to_stage: list of paths in the project directory to stage/copy to cache
-def add(to_stage):
-    pass
+def stage(to_stage):
+    if len(sys.argv) == 3:
+        arg1 = sys.argv[2]
+
+        if not os.path.isfile(arg1):
+            perror("add - could not find " + arg1)
+            exit(1)
+
+            # add to cache
+            buf = arg1.split('/')
+            base_name = buf[len(buf) - 1]
+
+            shutil.copyfile(arg1, CACHE_VOLUME+base_name)
+
+            # Update head reference in master volume
+            shutil.copyfile(arg1, HEAD_REF_MASTER+base_name)
+
+            assert os.path.isfile(CACHE_VOLUME+base_name), "Unable to write " + base_name + " to cache"
+            return
+        else:
+            perror("failed to stage - " + arg1)
+    perror("add command without <filename> is not supported yet")
 
 # View Commits
 def log(root):
@@ -166,22 +202,32 @@ def log(root):
 
 # Sync changes to the master volume 
 def sync(commit_msg):
-    # create commit id with create_hash
-    commit_id = NULL_SHA1
-    if len(os.listdirs(MASTER_VOLUME)):
-        commit_id = str(create_hash(CONFIG_USER_NAME, datetime.now(), commit_msg))
+    if len(sys.argv) == 3 and sys.argv[0] == '"' and sys.argv[-1] == '"':
+        arg1 = sys.argv[2].strip('\"')
 
-    # create directory for snapshot
-    print(commit_id)
-    snapshot = mkdir(os.path.join(MASTER_VOLUME, commit_id + '/'))
+        if len(arg1) == 0:
+            perror("commit message cannot be empty")
+            return
 
-    for filename in os.listdir(CACHE_VOLUME):
-        buf = filename.split('/')
-        base_name = buf[len(buf) - 1]
-        shutil.copyfile(filename, snapshot+base_name)
+        # create commit id with create_hash
+        commit_id = NULL_SHA1
+        if len(os.listdir(HEAD_REF_MASTER)):
+            commit_id = str(create_hash(CONFIG_USER_NAME, datetime.now(), commit_msg))
 
-        assert os.path.isfile(snapshot + base_name), "Unable to write " + base_name + " to master"
-    return 
+        # create directory for snapshot
+        print(commit_id)
+        snapshot = mkdir(os.path.join(HEAD_REF_MASTER, commit_id + '/'))
+
+        for filename in os.listdir(CACHE_VOLUME):
+            buf = filename.split('/')
+            base_name = buf[len(buf) - 1]
+            shutil.copyfile(filename, snapshot+base_name)
+
+            assert os.path.isfile(snapshot + base_name), "Unable to write " + base_name + " to master"
+        return 
+    else:
+        perror("sync command missing proper commit message")
+        print("usage: \n   sync <commit-msg> -- example: sync \"initial commit\"")
 
 ### === Driver === ###
 
@@ -195,9 +241,9 @@ start a VCS
 show changes
    diff                     Show current changes to files that were made
 stage changes
-   add <filename>           Stage changes
+   stage <filename>         Stage changes to cache
 sync changes
-   sync                     Sync changes to master
+   sync <commit-msg>        Sync changes to master
 view changelog
    log                      View changelog of commits 
 checkout a commit 
@@ -224,44 +270,18 @@ def main():
             usage()
         case "setup":
             setup()
-            print("roy: setup complete")
         case "diff":
             diff()
-        case "add":
-            if len(sys.argv) == 3:
-                arg1 = sys.argv[2]
-
-                if not os.path.isfile(arg1):
-                    perror("add - could not find " + arg1)
-                    exit(1)
-
-                # add to cache
-                buf = arg1.split('/')
-                base_name = buf[len(buf) - 1]
-
-                shutil.copyfile(arg1, CACHE_VOLUME+base_name)
-
-                assert os.path.isfile(CACHE_VOLUME+base_name), "Unable to write " + base_name + " to cache"
-                return
-            perror("add command without <filename> is not supported yet")
+        case "stage":
+            stage()
         case "sync":
-            if len(sys.argv) == 3:
-                arg1 = sys.argv[2]
-
-                if len(arg1) == 0:
-                    perror("commit message cannot be empty")
-                    return
-
-                sync(arg1)
-                return
-
-            perror("sync command missing proper commit message")
+            sync()
 
         # for debugging TODO remove
         case "clearcache":
             clear_cache_volume()
-        case "clearmaster":
-            clear_master_volume()
+        case "clearheadref":
+            clear_headref_master()
 
 if __name__ == "__main__":
     main()
